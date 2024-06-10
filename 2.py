d110729bcb60cda98ee810b50ae420463507a552
OtabekMambetov <166200744+OtabekMambetov@users.noreply.github.com>
2024-06-10 16:25:53 +0500

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,50 @@
+davlatlar_poytaxtlar = {
+    "O'zbekiston": "Toshkent",
+    "Qozog'iston": "Nur-Sulton",
+    "Qirg'iziston": "Bishkek",
+    "Tojikiston": "Dushanbe",
+    "Turkmaniston": "Ashxobod",
+    "Rossiya": "Moskva",
+    "AQSh": "Vashington",
+    "Xitoy": "Pekin",
+    "Yaponiya": "Tokio",
+    "Germaniya": "Berlin"
+}
+
+
+davlatlar = sorted(davlatlar_poytaxtlar.keys())
+print("Davlatlar:")
+for davlat in davlatlar:
+    print(davlat)
+
+# Poytaxtlarni alifbo ketma-ketligida chiqarish
+poytaxtlar = sorted(davlatlar_poytaxtlar.values())
+print("\nPoytaxtlar:")
+for poytaxt in poytaxtlar:
+    print(poytaxt)
+
+# Foydalanuvchidan davlatni kiritishni so'rash va poytaxtni chiqarish
+foydalanuvchi_davlati = input("\nDavlat nomini kiriting: ")
+if foydalanuvchi_davlati in davlatlar_poytaxtlar:
+    print(f"{foydalanuvchi_davlati}ning poytaxti {davlatlar_poytaxtlar[foydalanuvchi_davlati]}")
+else:
+    print("Bizda bunday ma'lumot yo'q")
+
+# Restoran menusi
+menu = {
+    "Osh": 20000,
+    "Shashlik": 15000,
+    "Lag'mon": 18000,
+    "Mastava": 12000,
+    "Somsa": 8000,
+    "Manti": 16000,
+    "Qozon kabob": 22000,
+    "Chuchvara": 14000,
+    "Salat": 10000,
+    "Non": 2000
+}
+
+# Foydalanuvchidan 3 ta ovqat buyurtma berishni so'rash
+buyurtmalar = []
+for i in range(3):
+menu
