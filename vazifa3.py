87ef131e9798c30a75f008712215855e500c1b9f
OtabekMambetov <166200744+OtabekMambetov@users.noreply.github.com>
2024-06-12 16:11:53 +0500

    Create vazifa3.py

---
+++ vazifa3.py
@@ -0,0 +1,15 @@
+#1
+ismlar=['Ali','Vali','Hasan','Olim']
+
+for ism in ismlar:
+    
+    print(ism)
+    
+print("Kod ", len(ismlar),"marta takrorlandi")
+
+#2
+
+sonlar=list(range(11,100,2))
+for son in sonlar:
+    print(son**3)
+
