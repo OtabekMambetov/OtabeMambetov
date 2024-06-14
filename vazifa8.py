4becd0d76a3f7f58212bee79cc934d27ca441926
OtabekMambetov <166200744+OtabekMambetov@users.noreply.github.com>
2024-06-14 15:53:09 +0500

    Create vazifa8.py

---
+++ vazifa8.py
@@ -0,0 +1,43 @@
+#1
+def tugilgan_yil():
+    ism = input("Ismingiz nima? ")
+    yosh = int(input("Yoshingiz nechida? "))
+    hozirgi_yil = 2024
+    tugilgan_yil = hozirgi_yil - yosh
+    print(f"{name}, siz {tugilgan_yil}-yilda tug'ilgansiz.")
+    
+#2
+
+def kvadrati_va_kubi():
+    son = int(input("Iltimos, biror son kiriting: "))
+    kvadrati = son** 2
+    kubi = son ** 3
+    print(f"{number} ning kvadrati {kvadrati}, kubi esa {kubi}.")
+#3
+def juft_yoki_toq():
+    son = int(input("Iltimos, biror son kiriting: "))
+    if number % 2 == 0:
+        print(f"{son} juft son.")
+    else:
+        print(f"{son} toq son.")
+    #4
+def son(x=None, y=2):
+    if x is None:
+        x = int(input("Birinchi sonni kiriting: "))
+    y = int(input("Ikkinchi sonni kiriting: ")) if y == 2 else y
+    if x > y:
+        print(f"Katta son: {x}")
+    elif y > x:
+        print(f"Katta son: {y}")
+    else:
+        print("Sonlar teng.")
+#5
+def toplam():
+    son = int(input("Iltimos, biror son kiriting: "))
+    for i in range(2, 11):
+        if number % i == 0:
+            print(f"{son} {i} ga qoldiqsiz bo'linadi.")
+        else:
+            print(f"{son} {i} ga qoldiqsiz bo'linmaydi.")
+
+
